1491ca8a6081f8a5846d44e5b12fc6edaf72b8c4
Abel Buechner-Mihaljevic <abel.buechner-mihaljevic@bosch.io>
2026-05-23 11:12:55 +0200

    blank lines

--- src/test/java/com/codemanship/DummyTest.java
+++ src/test/java/com/codemanship/DummyTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DummyTest {
+
     @Test
     void something(){
         assertTrue(true);
     }
+
 }
